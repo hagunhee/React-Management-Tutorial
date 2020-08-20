1aced3c716253a817cc394616be0f98f3df4cfee
hagunhee <hatchi10@gmail.com>
2020-08-21 03:00:02 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,43 +1,84 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Paper from '@material-ui/core/Paper';
 import Customer from './components/Customer';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles';
+import { render } from '@testing-library/react';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+})
+
 
 const customers = [
-  
+
   {
-  'id':1,
-  'image' : 'https://placeimg.com/64/64/1',
-  'name' : '홍길동',
-  'birthday' : '1991',
-  'gender' : '남자',
-  'jobs' : '교수'
-},
-{
-  'id':2,
-  'image' : 'https://placeimg.com/64/64/2',
-  'name' : '나동빈',
-  'birthday' : '1991',
-  'gender' : '남자',
-  'jobs' : '교수'
-},
-{
-  'id':3,
-  'image' : 'https://placeimg.com/64/64/3',
-  'name' : '하건희',
-  'birthday' : '1991',
-  'gender' : '남자',
-  'jobs' : '교수'
-}
+    'id': 1,
+    'image': 'https://placeimg.com/64/64/1',
+    'name': '홍길동',
+    'birthday': '1991',
+    'gender': '남자',
+    'jobs': '교수'
+  },
+  {
+    'id': 2,
+    'image': 'https://placeimg.com/64/64/2',
+    'name': '나동빈',
+    'birthday': '1991',
+    'gender': '남자',
+    'jobs': '교수'
+  },
+  {
+    'id': 3,
+    'image': 'https://placeimg.com/64/64/3',
+    'name': '하건희',
+    'birthday': '1991',
+    'gender': '남자',
+    'jobs': '교수'
+  }
 ]
 
 
-function App() {
-  return (
-    <div>
-      {customers.map (c =>{ return ( <Customer  key={c.id}  id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender}    jobs={c.jobs}/>)})}
-    </div>
-  )
+class App extends Component {
+
+  render() {
+
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {customers.map(c => { return (<Customer key={c.id} id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} jobs={c.jobs} />) })}
+          </TableBody>
+        </Table>
+      </Paper>
+
+    )
+  }
 }
 
-export default App;
+
+export default withStyles(styles)(App);
